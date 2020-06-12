342a1da8ef634204b1269984c19c3231c77e9f82
BurnQuid <sergpolevoi@yandex.ru>
2020-06-12 17:16:20 +0400

    цщц

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = 'telegram'  # токен самого бота, получается в Botfather
+BOT_TOKEN = '380185296:AAHJ17y2BZY-MLu22Fg-Z1pJqwCVNpI-KO0'  # токен самого бота, получается в Botfather
 PAYMENTS_PROVIDER_TOKEN = 'yandex:TEST:money'  # платёжный токен, получается в Botfather
 AI_TOKEN = 'AI by google'  # токен для подключения ИИ, получается в Dialogflow
